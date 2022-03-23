9b57c5263b94f52034efe893e74635487ae7d81f
francisuloko <francisuloko@yahoo.com>
2022-03-24 00:26:40 +0100

    Add share component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./home.css";
+import './home.css';
 import Topbar from '../../components/topbar/Topbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Feed from '../../components/feed/Feed';
