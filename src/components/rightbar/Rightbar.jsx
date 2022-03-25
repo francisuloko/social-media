d9231b316fa9f9e346f6502dff6b9eccf217bdc0
francisuloko <francisuloko@yahoo.com>
2022-03-25 21:28:39 +0100

    Add birthday celebrations

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,28 @@
+import { CardGiftcard } from '@mui/icons-material';
 import React from 'react';
 import './rightbar.css';
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer flex-center">
+          <CardGiftcard className="giftCard" />
+          <span className="birthdayText">Paola and 3 other friends have birthdays today</span>
+        </div>
+        <img className="rightbarAd" src="assets/persons/person-1.jpg" alt="" />
+        <h4 className="rightbarTitle">Friends Online</h4>
+        <ul className="rightbarFriendsList">
+          <li className="rightbarFriend flex-center">
+            <div className="rightbarProfileImageContainer">
+              {/* <img src="assets/persons/person-1.jpg" alt="" /> */}
+              <span className="rightbarOnline">
+                1
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
