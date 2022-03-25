3669581e852fc874fc3d0c2545bb9cae36168f3e
francisuloko <francisuloko@yahoo.com>
2022-03-25 22:26:17 +0100

    Add rightbar components

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -8,18 +8,33 @@ export default function Rightbar() {
       <div className="rightbarWrapper">
         <div className="birthdayContainer flex-center">
           <CardGiftcard className="giftCard" />
-          <span className="birthdayText">Paola and 3 other friends have birthdays today</span>
+          <span className="birthdayText">
+            Paola and 3 other friends have birthdays today
+          </span>
         </div>
         <img className="rightbarAd" src="assets/persons/person-1.jpg" alt="" />
         <h4 className="rightbarTitle">Friends Online</h4>
         <ul className="rightbarFriendsList">
           <li className="rightbarFriend flex-center">
-            <div className="rightbarProfileImageContainer">
-              {/* <img src="assets/persons/person-1.jpg" alt="" /> */}
-              <span className="rightbarOnline">
-                1
-              </span>
+            <div className="rightbarProfileImageContainer flex-center">
+              <img className="rightbarProfileImage" src="assets/persons/person-1.jpg" alt="" />
+              <span className="rightbarOnline" />
             </div>
+            <span className="rightbarUsername">John Doe</span>
+          </li>
+          <li className="rightbarFriend flex-center">
+            <div className="rightbarProfileImageContainer flex-center">
+              <img className="rightbarProfileImage" src="assets/persons/person-1.jpg" alt="" />
+              <span className="rightbarOnline" />
+            </div>
+            <span className="rightbarUsername">John Doe</span>
+          </li>
+          <li className="rightbarFriend flex-center">
+            <div className="rightbarProfileImageContainer flex-center">
+              <img className="rightbarProfileImage" src="assets/persons/person-1.jpg" alt="" />
+              <span className="rightbarOnline" />
+            </div>
+            <span className="rightbarUsername">John Doe</span>
           </li>
         </ul>
       </div>
