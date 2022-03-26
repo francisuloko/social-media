54fc1b4f8617696be8b7a094ef642cb152fedf8d
francisuloko <francisuloko@yahoo.com>
2022-03-26 22:52:41 +0100

    Add dynamic data to post component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,12 @@
-import { Favorite, MoreVert, ThumbUp } from '@mui/icons-material';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Favorite, MoreVert, ThumbUp } from '@mui/icons-material';
+import { Users } from '../../DummyData';
 import './post.css';
 
-export default function Post() {
+export default function Post({ post }) {
+  const user = Users.filter((user) => user.id === post.userId);
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -13,28 +17,43 @@ export default function Post() {
               alt=""
               className="postProfileImage"
             />
-            <span className="postUsername">User 1</span>
-            <span className="postDate">5 minutes ago</span>
+            <span className="postUsername">
+              { user[0].username }
+            </span>
+            <span className="postDate">
+              { post.date }
+            </span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">My first post!</span>
-          <img className="postImage" src="/assets/persons/profile1.jpg" alt="post" />
+          <span className="postText">
+            { post?.desc }
+          </span>
+          <img className="postImage" src={post.photo} alt="post" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <ThumbUp className="likeIcon" htmlColor="blue" />
             <Favorite className="likeIcon" htmlColor="red" />
-            <span className="postLikeCounter">32</span>
+            <span className="postLikeCounter">
+              { post.likes }
+            </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentCounter">9 comments</span>
+            <span className="postCommentCounter">
+              { post.comment }
+              comments
+            </span>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+Post.propTypes = {
+  post: PropTypes.string.isRequired,
+};
