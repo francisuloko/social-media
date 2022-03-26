1b89ba309df592ffb544167893b3baa734f94750
francisuloko <francisuloko@yahoo.com>
2022-03-26 22:41:06 +0100

    Remove desc from post data and fix linters error

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,47 +1,46 @@
 export const Users = [
   {
     id: 1,
-    profilePicture: "assets/persons/profile-1.jpg",
-    username: "John Doe",
+    profilePicture: 'assets/persons/profile-1.jpg',
+    username: 'John Doe',
   },
   {
     id: 2,
-    profilePicture: "assets/persons/profile-2.jpg",
-    username: "John Mark",
+    profilePicture: 'assets/persons/profile-2.jpg',
+    username: 'John Mark',
   },
   {
     id: 3,
-    profilePicture: "assets/persons/profile-3.jpg",
-    username: "Jane Doe",
-  }
-]
+    profilePicture: 'assets/persons/profile-3.jpg',
+    username: 'Jane Doe',
+  },
+];
 
 export const Posts = [
   {
     id: 1,
-    desc: "Post 1",
-    photo: "assets/posts/1.jpg",
-    date: "1 minute ago",
+    desc: 'Post 1',
+    photo: 'assets/person/profile1.jpg',
+    date: '1 minute ago',
     userId: 1,
     likes: 2,
     comment: 4,
   },
   {
     id: 2,
-    desc: "Post 2",
-    photo: "assets/posts/2.jpg",
-    date: "10 minute ago",
+    photo: 'assets/person/person-1.jpg',
+    date: '10 minute ago',
     userId: 2,
     likes: 22,
     comment: 6,
   },
   {
     id: 3,
-    desc: "Post 2",
-    photo: "assets/posts/2.jpg",
-    date: "10 minute ago",
+    desc: 'Post 2',
+    photo: 'assets/posts/2.jpg',
+    date: '10 minute ago',
     userId: 3,
     likes: 12,
     comment: 10,
   },
-]
+];
