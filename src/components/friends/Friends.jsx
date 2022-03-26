ea1470640abdc0b687493580223cb00e9241d312
francisuloko <francisuloko@yahoo.com>
2022-03-26 23:50:42 +0100

    Add dynamic data to friends component

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './friends.css';
 
-export default function Friends() {
+export default function Friends({ user }) {
   return (
-    <li className="sidebarListItem">
-      <RssFeed className="sidebarIcon" />
-      <span className="sidebarListItemText">Feed</span>
+    <li className="sidebarFriend">
+      <img
+        src={user.profilePicture}
+        alt=""
+        className="sidebarFriendImage"
+      />
+      <span className="sidebarFriendName">
+        { user.username }
+      </span>
     </li>
   );
 }
+
+Friends.propTypes = {
+  user: PropTypes.string.isRequired,
+};
