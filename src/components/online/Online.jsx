1e5b0903c278965fccb53d41c5185fe958d21dd8
francisuloko <francisuloko@yahoo.com>
2022-03-26 23:33:40 +0100

    Add dynamic data to online friends component

---
+++ src/components/online/Online.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './online.css';
+
+export default function Online({ user }) {
+  return (
+    <li className="rightbarFriend flex-center">
+      <div className="rightbarProfileImageContainer flex-center">
+        <img
+          className="rightbarProfileImage"
+          src={user.profilePicture}
+          alt=""
+        />
+        <span className="rightbarOnline" />
+      </div>
+      <span className="rightbarUsername">{ user.username }</span>
+    </li>
+  );
+}
+
+Online.propTypes = {
+  user: PropTypes.string.isRequired,
+};
