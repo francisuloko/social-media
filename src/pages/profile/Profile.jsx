b009a211fe85eb0c0ec06dbbddf5e7b839367003
francisuloko <francisuloko@yahoo.com>
2022-03-27 01:10:48 +0100

    Add profile basic structure

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './profile.css';
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop"></div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
