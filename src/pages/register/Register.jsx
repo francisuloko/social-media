dc242962179b76e59dbd3ac31edef58ec08d0204
francisuloko <francisuloko@yahoo.com>
2022-03-27 22:50:27 +0100

    Add register component

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './register.css';
+
+export default function Register() {
+  return (
+    <div>
+      <div className="login flex-center">
+        <div className="loginWrapper">
+          <div className="loginLeft flex-center">
+            <h3 className="loginLogo">Logo</h3>
+            <span className="loginDescription">
+              Connect with friends on Soso!
+            </span>
+          </div>
+          <div className="loginRight">
+            <div className="loginBox">
+              <input type="email" placeholder="Email" className="loginInput" />
+              <input type="password" placeholder="Password" className="loginInput" />
+              <button type="button" className="loginButton">Log In</button>
+              <span className="loginForget">Forgot Password?</span>
+              <button type="button" className="loginRegister">Create a new account.</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
